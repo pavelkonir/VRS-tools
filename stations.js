697e09253947deef2923430d02318f304100199d
Pavel Koníř <pavel@konir.cz>
2017-06-08 14:38:07 +0200

    Add meteo layer

--- stations.js
+++ stations.js
@@ -8,6 +8,61 @@ if(VRS && VRS.globalDispatch && VRS.serverConfig) {
                   var map = pageSettings.mapPlugin.getNative();
                   var markers = [];
                   var showStations = true;
+                  var showMeteo = false;
+                  var lastDatePart = "";
+                  var meteoLayerTemp = null;
+
+                  Date.prototype.utcSpecial = function() {
+                    var mm = this.getUTCMonth() + 1;
+                    var dd = this.getUTCDate();
+                    var hh = this.getUTCHours();
+                    var min = Math.floor(this.getUTCMinutes()/10)*10;
+
+                    return [this.getFullYear(),
+                      (mm>9 ? '' : '0') + mm,
+                      (dd>9 ? '' : '0') + dd,
+                      '.',
+                      (hh>9 ? '' : '0') + hh,
+                      (min>9 ? '' : '0') + min
+                    ].join('');
+                  };
+
+
+                  var imageBounds = new google.maps.LatLngBounds(
+                      new google.maps.LatLng(47.09,10.06),
+                      new google.maps.LatLng(51.889,20.223));
+
+                  var overlayOpts = {
+                    opacity:0.6
+                  }
+
+
+                  var meteoLayer = function() {
+                    if (showMeteo) {
+                      fileNameDatePart = new Date().utcSpecial();
+
+                      if (lastDatePart != fileNameDatePart) {
+                        if (meteoLayerTemp != null) {
+                          meteoLayerTemp.setMap(null);
+                        }
+
+                        meteoLayerOverlay = new google.maps.GroundOverlay(
+                            'http://radar.bourky.cz/data/pacz2gmaps.z_max3d.'
+                            + fileNameDatePart + '.0.png',
+                            imageBounds, overlayOpts);
+                        meteoLayerOverlay.setMap(map);
+
+                        meteoLayerTemp = meteoLayerOverlay;
+                      }
+                    } else {
+                      if (meteoLayerTemp != null) {
+                        meteoLayerTemp.setMap(null);
+                        meteoLayerTemp = null;
+                      }
+                    }
+                  }
+
+
 
                   function CzadsbControlRecivers(controlDiv, map) {
 
@@ -29,6 +84,30 @@ if(VRS && VRS.globalDispatch && VRS.serverConfig) {
                     receiversUIMenu.addEventListener('click', function() {
                       showStations = !showStations;
                       loadStations();
+                    });
+
+                  }
+
+                  function MeteoControlRecivers(controlDiv, map) {
+
+                    var meteoUIMenu = document.createElement('div');
+                    meteoUIMenu.className = "vrsMenu";
+                    meteoUIMenu.style.paddingRight = "10px";
+                    controlDiv.appendChild(meteoUIMenu);
+
+
+                    var meteoUI = document.createElement('div');
+                    meteoUI.className = "mapButton";
+                    meteoUIMenu.appendChild(meteoUI);
+
+                    var meteoText = document.createElement('span');
+                    meteoText.innerHTML = 'Meteo';
+                    meteoUI.appendChild(meteoText);
+
+                    // Setup the click event listeners
+                    meteoUIMenu.addEventListener('click', function() {
+                      showMeteo = !showMeteo;
+                      meteoLayer();
                     });
 
                   }
@@ -79,16 +158,28 @@ if(VRS && VRS.globalDispatch && VRS.serverConfig) {
                   }
 
                   loadStations();
+                  meteoLayer();
 
                   var receiversControlDiv = document.createElement('div');
                   var centerControl = new CzadsbControlRecivers(receiversControlDiv, map);
 
                   receiversControlDiv.index = 1;
-                  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(receiversControlDiv);
+                  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(receiversControlDiv);
+
+
+                  var meteoControlDiv = document.createElement('div');
+                  var meteoControl = new MeteoControlRecivers(meteoControlDiv, map);
+
+                  meteoControlDiv.index = 2;
+                  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(meteoControlDiv);
 
                   window.setInterval(function () {
                     loadStations();
                   }, 60000 * 10);
+
+                  window.setInterval(function () {
+                    meteoLayer();
+                  }, 60000 * 2);
                 }
             });
         }
