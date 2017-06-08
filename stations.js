d4d159376924563842236987b77b6ac1a84b7003
Pavel Koníř <pavel@konir.cz>
2017-06-08 12:01:26 +0200

    Added stations button and images for markers

--- stations.js
+++ stations.js
@@ -4,51 +4,91 @@ if(VRS && VRS.globalDispatch && VRS.serverConfig) {
         if(bootStrap.hookMapInitialised) {
             bootStrap.hookMapInitialised(function(pageSettings) {
                 if(pageSettings.mapPlugin && pageSettings.mapPlugin.getNativeType() === 'GoogleMaps') {
+
                   var map = pageSettings.mapPlugin.getNative();
                   var markers = [];
+                  var showStations = true;
+
+                  function CzadsbControlRecivers(controlDiv, map) {
+
+                    var receiversUIMenu = document.createElement('div');
+                    receiversUIMenu.className = "vrsMenu";
+                    receiversUIMenu.style.paddingRight = "10px";
+                    controlDiv.appendChild(receiversUIMenu);
+
+
+                    var receiversUI = document.createElement('div');
+                    receiversUI.className = "mapButton";
+                    receiversUIMenu.appendChild(receiversUI);
+
+                    var receiverText = document.createElement('span');
+                    receiverText.innerHTML = 'Stations';
+                    receiversUI.appendChild(receiverText);
+
+                    // Setup the click event listeners
+                    receiversUIMenu.addEventListener('click', function() {
+                      showStations = !showStations;
+                      loadStations();
+                    });
+
+                  }
 
                   var loadStations = function() {
-                    $.ajax({
-                      type: "GET",
-                      url: "stations.json",
-                      data: '{}',
-                      contentType: "application/json; charset=utf-8",
-                      dataType: "json",
-                      success: function (response) {
+                    if (showStations) {
+                      $.ajax({
+                        type: "GET",
+                        url: "stations.json",
+                        data: '{}',
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (response) {
 
-                        for (var i = 0; i < markers.length; i++) {
-                          markers[i].setMap(null);
+                          for (var i = 0; i < markers.length; i++) {
+                            markers[i].setMap(null);
+                          }
+
+                          markers = [];
+
+                          response.data.forEach(function (entry) {
+
+                            marker = new google.maps.Marker({
+                              map: map,
+                              position: entry.position,
+                              icon: entry.icon,
+                              title: entry.title
+                            });
+
+                            markers.push(marker);
+                          })
+
+                        },
+                        failure: function (response) {
+                          console.error(response);
+                        },
+                        error: function (response) {
+                          console.error(response);
                         }
-
-                        markers = [];
-
-                        response.data.forEach(function(entry) {
-
-                          marker = new google.maps.Marker({
-                            map: map,
-                            position: entry.position,
-                            icon: entry.icon,
-                            title: entry.title
-                          });
-
-                          markers.push(marker);
-                        })
-
-                      },
-                      failure: function (response) {
-                        console.error(response);
-                      },
-                      error: function (response) {
-                        console.error(response);
+                      })
+                    } else {
+                      for (var i = 0; i < markers.length; i++) {
+                        markers[i].setMap(null);
                       }
-                    })
+
+                      markers = [];
+                    }
                   }
 
                   loadStations();
 
+                  var receiversControlDiv = document.createElement('div');
+                  var centerControl = new CzadsbControlRecivers(receiversControlDiv, map);
+
+                  receiversControlDiv.index = 1;
+                  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(receiversControlDiv);
+
                   window.setInterval(function () {
                     loadStations();
-                  }, 5000);
+                  }, 60000 * 10);
                 }
             });
         }
